b293c6af5a737a5ffb5b83bdc3feb06deffe8772
DragonGriffon2003 <52962345+DragonGriffon2003@users.noreply.github.com>
2019-07-17 18:00:52 +0530

    Initial commit

---
+++ Assets/Click.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using TMPro;
+
+public class Click : MonoBehaviour, IPointerClickHandler, IPointerDownHandler, IPointerEnterHandler, IPointerExitHandler
+{
+	public Material material;
+	public TextMeshPro text;
+
+	/*
+	public void OnMouseOver()
+	{
+		material.color = Color.red;
+	}
+
+	private void OnMouseExit()
+	{
+		material.color = Color.green;
+
+	}*/
+
+	public void ChangeColor()
+	{
+		material.color = Color.cyan;
+	}
+
+	public void OnPointerClick(PointerEventData eventData)
+	{
+		text.text = "Clicked";
+	}
+
+	public void OnPointerEnter(PointerEventData pointerEventData)
+	{
+		text.text = "Hovering";
+		material.color = Color.cyan;
+	}
+
+
+	public void OnPointerExit(PointerEventData eventData)
+	{
+		text.text = "Normal";
+		material.color = Color.red;
+	}
+
+
+	public void OnPointerDown(PointerEventData eventData)
+	{
+
+	}
+
+}
---
+++ Assets/MoveCamera.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCamera : MonoBehaviour
+{
+
+	public CharacterController myCharacterController;
+	public float walkSpeed;
+
+
+	private void Update()
+	{
+		// Get Horizontal and Vertical Input
+		float horizontalInput = Input.GetAxis("Horizontal");
+		float verticalInput = Input.GetAxis("Vertical");
+	
+		// Calculate the Direction to Move based on the tranform of the Player
+		Vector3 moveDirectionForward = transform.forward * verticalInput * walkSpeed * Time.deltaTime;
+		Vector3 moveDirectionSide = transform.right * horizontalInput * walkSpeed * Time.deltaTime;
+
+		// Apply Movement to Player
+		myCharacterController.SimpleMove(moveDirectionForward + moveDirectionSide);
+	}
+
+}
