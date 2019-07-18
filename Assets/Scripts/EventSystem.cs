a9bf60a36781e4d9c60ea3b6f28a982857fde3ae
DragonGriffon2003 <ishana.rama@gmail.com>
2019-07-18 17:20:33 +0530

    Obstacle#1 Start Point

---
+++ Assets/Scripts/EventSystem.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class EventSystem : MonoBehaviour, IPointerClickHandler
+{
+	public MoveCamera moveCamera;
+
+	public void OnPointerClick(PointerEventData pointerEventData)
+	{
+		if (pointerEventData.pointerCurrentRaycast.gameObject.tag == "StartButton")
+		{
+			//Camera.main.GetComponent<MoveCamera>().canMove = true;
+			moveCamera.canMove = true;
+		}
+	}
+}
---
+++ Assets/Scripts/MoveCamera.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCamera : MonoBehaviour
+{
+	//Variables
+	public float speedH = 2.0f;
+	public float speedV = 2.0f;
+
+	private float yaw = 0.0f;
+	private float pitch = 0.0f;
+
+	public bool canMove = false;
+	public float speed = 6.0f;
+	public float jumpSpeed = 8.0f;
+	public float gravity = 18f;
+	private Vector3 moveDirection = Vector3.zero;
+
+	void Update()
+	{
+		CharacterController controller = GetComponent<CharacterController>();
+		// is the controller on the ground?
+		if (controller.isGrounded)
+		{
+			//Feed moveDirection with input.
+			moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+			moveDirection = transform.TransformDirection(moveDirection);
+			//Multiply it by speed.
+			moveDirection *= speed;
+			//Jumping
+			if (Input.GetButton("Jump"))
+				moveDirection.y = jumpSpeed;
+
+		}
+		//Applying gravity to the controller
+		moveDirection.y -= gravity * Time.deltaTime;
+		
+		//Making the character move only if this bool funtion is true
+		if (canMove == true)
+		{
+			Debug.Log("canMove = true");
+			controller.Move(moveDirection * Time.deltaTime);
+		}
+		if (Input.GetMouseButton(1))
+		{
+			yaw += speedH * Input.GetAxis("Mouse X");
+			pitch -= speedV * Input.GetAxis("Mouse Y");
+			transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+		}
+
+	}
+}
