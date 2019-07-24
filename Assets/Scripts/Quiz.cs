83047ad1b161fd9854d020e29225a2c4a52f296a
DragonGriffon2003 <52962345+DragonGriffon2003@users.noreply.github.com>
2019-07-24 18:19:44 +0530

    Integrating the quiz to use only one script
    I will be revamping the quiz module to use only one script.
    The main script controller is "Quiz".

---
+++ Assets/Scripts/Quiz.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quiz : MonoBehaviour
+{
+	//Find all gameobejct in Start and add a function to them
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
